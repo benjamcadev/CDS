0c9a03f759d7bb288fa04622f6ea8b6db372dfce
Cortes Alvarez Benjamin (Contratista-Salvador) <BCort015@contratistas.codelco.cl>
2024-06-09 01:09:14 -0400

    se agrega nombre de usuario en sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import logoPsinet from '/src/Logo-PSINet.png'
 import logoPsinetSmall from '/src/Logo-PSINet_small.png'
 
 //REACT ROUTER
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 //IMPORTANDO CONTEXT DE AUTH
 import { useAuth } from '../context/AuthContext'
@@ -26,21 +26,21 @@ export default function Sidebar() {
 
   useEffect(() => {
 
-    if(window.innerWidth <= 768) {
+    if (window.innerWidth <= 768) {
       setOpen(false)
     }
 
-  },[])
+  }, [])
 
   const Menus = [
     { title: "Dashboard", spacing: true, link: "/" },
-    { title: "Vale Salida", icon: <FaFileArrowDown />, spacing: true, link: "/vale-salida"},
-    { title: "Vale Entrada", icon: <FaFileArrowUp />, link: "/"},
-    { title: "Ver Vales Salida", icon: <FaFileSignature />, link: "/"},
-   
+    { title: "Vale Salida", icon: <FaFileArrowDown />, spacing: true, link: "/vale-salida" },
+    { title: "Vale Entrada", icon: <FaFileArrowUp />, link: "/" },
+    { title: "Ver Vales Salida", icon: <FaFileSignature />, link: "/" },
+
     {
       title: "Materiales",
-      icon: <FaClipboardList />, 
+      icon: <FaClipboardList />,
       link: "/",
       spacing: true,
       submenu: true,
@@ -49,69 +49,79 @@ export default function Sidebar() {
         { title: "Crear Material" },
       ],
     },
-    
+
     { title: "Reportes", icon: <FaFileInvoice />, link: "/" },
     {
       title: "Opciones",
-      icon: <FaGear />, 
+      icon: <FaGear />,
       link: "/",
       spacing: true,
       submenu: true,
       submenuItems: [
         { title: "Usuarios" },
-        { title: "Sistema" },
+        { title: "Registrar Usuarios", link: "/register" },
+        { title: "Sistema", },
       ],
     },
-    { title: "Cerrar Sesion", icon: <FaArrowRightFromBracket />, link: "", functionOnClick: () => {logout()}  },
+    { title: "Cerrar Sesion", icon: <FaArrowRightFromBracket />, link: "", functionOnClick: () => { logout() } },
   ]
 
 
   return (
 
     <>
-    {isAuthenticated ? 
-    
-    <div className={`bg-gray-800 h-screen p-5 pt-8 mr-4 ${open ? 'w-72' : 'w-20'} h-screen relative duration-300 rounded-md`}>
-      <FaAngleLeft
-        className={` bg-white text-gray-800 text-3xl rounded-full absolute -right-3 top-9 border border-gray-800 cursor-pointer ${!open && "rotate-180"}`}
-        onClick={() => setOpen(!open)}
-      />
-      <div className=" inline-flex">
-        {open ? <img className='h-8 w-17 duration-500 md:h-12  ' src={logoPsinet} /> : <img className='h-8 w-17 duration-500 md:h-12  ' src={logoPsinetSmall} />}
-      </div>
+      {isAuthenticated ?
 
-      <ul>
-        {Menus.map((menu, index) => (
-          <div key={index}>
-         
-            <li key={index} className={` text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-gray-600 rounded-md ${menu.spacing ? "mt-9" : "mt-2"} `}>
-              <span  className="text-2xl block float-left">
-                {menu.icon ? menu.icon : <FaBars /> }
-              </span>
-               <span className={` text-base font-medium flex-1 duration-200 ${!open && "hidden"}`} >
-               <Link to={menu.link} onClick={menu.functionOnClick}>   {menu.title} </Link>
-              </span>
-              {menu.submenu && open && (
-                <FaChevronDown className={`${submenuOpen && "rotate-180"} duration-300`} onClick={() => setSubmenuOpen(!submenuOpen)}/>
-              )}
-            </li>
-
-            {menu.submenu && submenuOpen && open && (
-              <ul>
-                {menu.submenuItems.map((submenuItem, index) => (
-                  <li key={index} className=" text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-gray-600 rounded-md">
-                    {submenuItem.title}
-                  </li>
-                ))}
-              </ul>
-            )}
+        <div className={`bg-gray-800 h-screen p-5 pt-8 mr-4 ${open ? 'w-72' : 'w-20'} h-screen relative duration-300 rounded-md`}>
+          <FaAngleLeft
+            className={` bg-white text-gray-800 text-3xl rounded-full absolute -right-3 top-9 border border-gray-800 cursor-pointer ${!open && "rotate-180"}`}
+            onClick={() => setOpen(!open)}
+          />
+          <div className=" inline-flex">
+            {open ? <img className='h-8 w-17 duration-500 md:h-12  ' src={logoPsinet} /> : <img className='h-8 w-17 duration-500 md:h-12  ' src={logoPsinetSmall} />}
           </div>
-        ))}
-      </ul>
+
+          {open ?
+            <div>
+              <p className=" text-gray-300 text-xs font-medium">Bienvenido !</p>
+              <p className=" text-gray-300 font-medium">{user.nombre}</p>
+            </div>
+            :
+            ''
+          }
+
+          <ul>
+            {Menus.map((menu, index) => (
+              <div key={index}>
+
+                <li key={index} className={` text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-gray-600 rounded-md ${menu.spacing ? "mt-9" : "mt-2"} `}>
+                  <span className="text-2xl block float-left">
+                    {menu.icon ? menu.icon : <FaBars />}
+                  </span>
+                  <span className={` text-base font-medium flex-1 duration-200 ${!open && "hidden"}`} >
+                    <Link to={menu.link} onClick={menu.functionOnClick}>   {menu.title} </Link>
+                  </span>
+                  {menu.submenu && open && (
+                    <FaChevronDown className={`${submenuOpen && "rotate-180"} duration-300`} onClick={() => setSubmenuOpen(!submenuOpen)} />
+                  )}
+                </li>
+
+                {menu.submenu && submenuOpen && open && (
+                  <ul>
+                    {menu.submenuItems.map((submenuItem, index) => (
+                      <li key={index} className=" text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-gray-600 rounded-md">
+                        <Link to={submenuItem.link} onClick={submenuItem.functionOnClick}>   {submenuItem.title} </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+          </ul>
 
 
-    </div> : ''}
+        </div> : ''}
     </>
-    
+
   )
 }
