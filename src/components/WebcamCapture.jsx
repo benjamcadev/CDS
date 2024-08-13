3ff6a9729a842e95e57c03111e58d6b892b2cf24
francodev7 <francisco.pereira16@inacapmail.cl>
2024-08-13 08:17:59 -0400

    webcam fix

--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -6,12 +6,7 @@ import CameraAltIcon from '@mui/icons-material/CameraAlt';
 const WebcamCapture = ({ setImage }) => {
   const [deviceId, setDeviceId] = useState(null);
   const [devices, setDevices] = useState([]);
-
-  const handleDevices = useCallback(
-    mediaDevices =>
-      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
-    [setDevices]
-  );
+  
   const webcamRef = useRef(null);
   
   const capture = useCallback(() => {
@@ -19,20 +14,22 @@ const WebcamCapture = ({ setImage }) => {
     setImage(imageSrc); // Aquí enviamos la imagen capturada al componente padre
   }, [webcamRef, setImage]);
 
+  const handleDevices = useCallback(
+    (mediaDevices) => {
+      const videoDevices = mediaDevices.filter(({ kind }) => kind === "videoinput");
+      setDevices(videoDevices);
+      console.log("Available video devices:", videoDevices); // Imprime en la consola los dispositivos de video
+    },
+    [setDevices]
+  );
+
   useEffect(() => {
     navigator.mediaDevices.enumerateDevices().then(handleDevices);
   }, [handleDevices]);
 
   useEffect(() => {
-    // Filtrar para seleccionar la cámara trasera
-    const backCamera = devices.find((device) =>
-      device.label.toLowerCase().includes("back")
-    );
-
-    if (backCamera) {
-      setDeviceId(backCamera.deviceId);
-    } else if (devices.length > 0) {
-      // Si no se encuentra una cámara con "back", selecciona el primer dispositivo
+    if (devices.length > 0) {
+      // Aquí selecciona el primer dispositivo, puedes cambiarlo después de ver los nombres
       setDeviceId(devices[0].deviceId);
     }
   }, [devices]);
