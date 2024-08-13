fc1773911f5c6f820ab60cd360e68efd66633b86
francodev7 <francisco.pereira16@inacapmail.cl>
2024-08-13 09:10:49 -0400

    camera tablet find

--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -4,35 +4,35 @@ import Webcam from 'react-webcam';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 const WebcamCapture = ({ setImage }) => {
+
   const [deviceId, setDeviceId] = useState(null);
-  const [devices, setDevices] = useState([]);
-  
   const webcamRef = useRef(null);
   
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
-    setImage(imageSrc); // Aquí enviamos la imagen capturada al componente padre
-  }, [webcamRef, setImage]);
+    setImage(imageSrc);
+  }, [setImage]);
 
   const handleDevices = useCallback(
     (mediaDevices) => {
       const videoDevices = mediaDevices.filter(({ kind }) => kind === "videoinput");
-      setDevices(videoDevices);
-      console.log("Available video devices:", videoDevices); // Imprime en la consola los dispositivos de video
+      
+      // Buscar el dispositivo que tiene el label deseado
+      const rearCamera = videoDevices.find(device => device.label.includes('Rear Integrated Webcam'));
+
+      if (rearCamera) {
+        setDeviceId(rearCamera.deviceId);
+      } else if (videoDevices.length > 0) {
+        // Si no encuentra la cámara trasera, usar el primer dispositivo
+        setDeviceId(videoDevices[0].deviceId);
+      }
     },
-    [setDevices]
+    []
   );
 
   useEffect(() => {
     navigator.mediaDevices.enumerateDevices().then(handleDevices);
   }, [handleDevices]);
-
-  useEffect(() => {
-    if (devices.length > 0) {
-      // Aquí selecciona el primer dispositivo, puedes cambiarlo después de ver los nombres
-      setDeviceId(devices[0].deviceId);
-    }
-  }, [devices]);
 
   return (
     //<div style={{ textAlign: 'center' }}>
