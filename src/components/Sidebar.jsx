f19f5f5e70f1401a6505d959c716404cfc1d79ef
Cortes Alvarez Benjamin (Contratista-Salvador) <BCort015@contratistas.codelco.cl>
2025-03-31 14:46:28 -0300

    Fix submenu opciones y reportes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,25 +16,26 @@ import logoPsinetSmall from '../public/images/Logo-PSINet_small.png';
 export default function Sidebar() {
   const [open, setOpen] = useState(true);
   const [submenuOpen, setSubmenuOpen] = useState(false);
+  const [submenuReportesOpen, setSubmenuReportesOpen] = useState(false);
   const { user, logout, isAuthenticated } = useAuth();
   const location = useLocation();
-  
+
   useEffect(() => {
     if (window.innerWidth <= 768) {
       setOpen(false);
     }
   }, []);
-  
+
 
   const Menus = [
     { title: "Dashboard", spacing: true, link: "/" },
-    { title: "Vale Salida", icon: <FaFileArrowDown />, spacing: true, link: "/vale-salida" , visible: user?.tipoUser === 1 || user?.tipoUser === 3 },
+    { title: "Vale Salida", icon: <FaFileArrowDown />, spacing: true, link: "/vale-salida", visible: user?.tipoUser === 1 || user?.tipoUser === 3 },
 
-  
-  
-    { title: "Vale Entrada", icon: <FaFileArrowUp />, link: "/vale-entrada", visible: user?.tipoUser === 1},
 
-    {title: "Cotizacion", icon: <FaFileInvoiceDollar/>, link: "/cotizacion", visible: user?.tipoUser === 1},
+
+    { title: "Vale Entrada", icon: <FaFileArrowUp />, link: "/vale-entrada", visible: user?.tipoUser === 1 },
+
+    { title: "Cotizacion", icon: <FaFileInvoiceDollar />, link: "/cotizacion", visible: user?.tipoUser === 1 },
     //{ title: "Ver Vales Salida", icon: <FaFileSignature />, link: "/ver-vales-salida" },
 
     {
@@ -43,10 +44,23 @@ export default function Sidebar() {
       link: "/Articulos",
       spacing: true,
       visible: user?.tipoUser === 1 || user?.tipoUser === 3
-     
+
     },
 
-    { title: "Reportes", icon: <FaFileInvoice />, link: "/reportes" , visible: user?.tipoUser === 1 },
+    {
+      title: "Reportes",
+      icon: <FaFileInvoice />,
+      link: "/reportes",
+      spacing: true,
+      submenu: true,
+      visible: user?.tipoUser === 1,
+      submenuItems: [
+        { title: "Listado Cotizacion", link: "/listado-cotizacion" },
+        { title: "Listado Vale Entrada", link: "/listado-vale-entrada" },
+        { title: "Listado Vale Salida", link: "/listado-vale-salida" },
+      ],
+    },
+
 
     {
       title: "Opciones",
@@ -64,16 +78,16 @@ export default function Sidebar() {
     { title: "Cerrar Sesion", icon: <FaArrowRightFromBracket />, link: "", functionOnClick: logout },
   ];
 
-   // Filtrar menús visibles antes de renderizar
-   const filteredMenus = Menus.filter(menu => menu.visible !== false);
+  // Filtrar menús visibles antes de renderizar
+  const filteredMenus = Menus.filter(menu => menu.visible !== false);
 
 
   return (
     <>
       {isAuthenticated && user.usuario != 'no-sidebar' ? (
-      
+
         <div style={{ height: '185vh' }} className={`bg-gray-800 h-screen flex flex-col  overflow-y-auto p-5 pt-8 mr-4  ${open ? 'w-[64]' : 'w-20 no-scrollbar'} h-screen relative duration-300`}>
-          
+
           <div className="inline-flex">
             {open ? (
               <img className='h-8 duration-500 md:h-12' src={logoPsinet} alt="Logo" />
@@ -96,18 +110,22 @@ export default function Sidebar() {
               <div key={index}>
                 <Link to={menu.link} onClick={menu.functionOnClick}>
                   <li className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-gray-600 rounded-md ${menu.spacing ? "mt-9" : "mt-2"} ${location.pathname === menu.link ? "bg-blue-600" : ""}`}>
-                    <span className="text-2xl block float-left">  
+                    <span className="text-2xl block float-left">
                       {menu.icon ? menu.icon : <FaBars />}
                     </span>
                     <span className={`text-base font-medium flex-1 duration-200 ${!open && "hidden"}`}>
                       {menu.title}
                     </span>
-                    {menu.submenu && open && (
+                    {menu.submenu && open && menu.title == 'Opciones' && (
                       <FaChevronDown className={`${submenuOpen && "rotate-180"} duration-300`} onClick={() => setSubmenuOpen(!submenuOpen)} />
+                    )}
+
+                    {menu.submenu && open && menu.title == 'Reportes' && (
+                      <FaChevronDown className={`${submenuReportesOpen && "rotate-180"} duration-300`} onClick={() => setSubmenuReportesOpen(!submenuReportesOpen)} />
                     )}
                   </li>
                 </Link>
-                {menu.submenu && submenuOpen && open && (
+                {menu.submenu && submenuOpen && open && menu.title == 'Opciones' &&  (
                   <ul>
                     {menu.submenuItems.map((submenuItem, subIndex) => (
                       <li key={subIndex} className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-gray-600 rounded-md`}>
@@ -116,25 +134,36 @@ export default function Sidebar() {
                     ))}
                   </ul>
                 )}
+
+                {menu.submenu && submenuReportesOpen && open && menu.title == 'Reportes' &&  (
+                  <ul>
+                    {menu.submenuItems.map((submenuItem, subIndex) => (
+                      <li key={subIndex} className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-gray-600 rounded-md`}>
+                        <Link to={submenuItem.link}>{submenuItem.title}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+
               </div>
             ))}
           </ul>
           {open && (
             <div className="mt-6">
               <p className="text-gray-400 text-xs font-medium">© 2024</p>
-              <a 
-                href="https://github.com/benjamcadev" 
-                className="text-gray-400 text-xs font-medium" 
-                target="_blank" 
+              <a
+                href="https://github.com/benjamcadev"
+                className="text-gray-400 text-xs font-medium"
+                target="_blank"
                 rel="noopener noreferrer"
-               >
+              >
                 By benjamcadev 👨🏻‍💻
               </a>
-              <div>  
-                <a 
-                  href="https://github.com/FrancoDev7" 
-                  className="text-gray-400 text-xs font-medium" 
-                  target="_blank" 
+              <div>
+                <a
+                  href="https://github.com/FrancoDev7"
+                  className="text-gray-400 text-xs font-medium"
+                  target="_blank"
                   rel="noopener noreferrer"
                 >
                   By FrancoDev7 👨🏻‍💻
